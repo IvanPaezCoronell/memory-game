8207f441f933dc700724fb75401d8d6781513813
Ivan Paez Coronell <ipaezcoronell@outlook.com>
2023-07-04 11:22:10 -0500

    fix: test 01 - imagenes no aparecen en el deploy

--- src/data.js
+++ src/data.js
@@ -1,60 +1,60 @@
 export const imgs = [
 	{
 		id: 1,
-		img: '/public/img/1.svg',
+		img: '/public/1.svg',
 		alt: 'first img',
 	},
 
 	{
 		id: 2,
-		img: '/public/img/2.svg',
+		img: '/public/2.svg',
 		alt: 'second img',
 	},
 	{
 		id: 3,
-		img: '/public/img/3.svg',
+		img: '/public/3.svg',
 		alt: 'third img',
 	},
 
 	{
 		id: 4,
-		img: '/public/img/4.svg',
+		img: '/public/4.svg',
 		alt: 'fourth img',
 	},
 
 	{
 		id: 5,
-		img: '/public/img/5.svg',
+		img: '/public/5.svg',
 		alt: 'fifth img',
 	},
 
 	{
 		id: 6,
-		img: '/public/img/6.svg',
+		img: '/public/6.svg',
 		alt: 'sixth img',
 	},
 
 	{
 		id: 7,
-		img: '/public/img/7.svg',
+		img: '/public/7.svg',
 		alt: 'seventh img',
 	},
 
 	{
 		id: 8,
-		img: '/public/img/8.svg',
+		img: '/public/8.svg',
 		alt: 'eighth img',
 	},
 
 	{
 		id: 9,
-		img: '/public/img/9.svg',
+		img: '/public/9.svg',
 		alt: 'ninth img',
 	},
 
 	{
 		id: 10,
-		img: '/public/img/10.svg',
+		img: '/public/10.svg',
 		alt: 'tenth img',
 	},
 ];
