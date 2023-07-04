368d4c180cad560f44bd842d4fbf87da5f54cb58
Ivan Paez Coronell <ipaezcoronell@outlook.com>
2023-07-04 11:25:13 -0500

    fix: test 02 - imagenes no aparecen en el deploy

--- src/data.js
+++ src/data.js
@@ -1,60 +1,60 @@
 export const imgs = [
 	{
 		id: 1,
-		img: '/public/1.svg',
+		img: '/1.svg',
 		alt: 'first img',
 	},
 
 	{
 		id: 2,
-		img: '/public/2.svg',
+		img: '/2.svg',
 		alt: 'second img',
 	},
 	{
 		id: 3,
-		img: '/public/3.svg',
+		img: '/3.svg',
 		alt: 'third img',
 	},
 
 	{
 		id: 4,
-		img: '/public/4.svg',
+		img: '/4.svg',
 		alt: 'fourth img',
 	},
 
 	{
 		id: 5,
-		img: '/public/5.svg',
+		img: '/5.svg',
 		alt: 'fifth img',
 	},
 
 	{
 		id: 6,
-		img: '/public/6.svg',
+		img: '/6.svg',
 		alt: 'sixth img',
 	},
 
 	{
 		id: 7,
-		img: '/public/7.svg',
+		img: '/7.svg',
 		alt: 'seventh img',
 	},
 
 	{
 		id: 8,
-		img: '/public/8.svg',
+		img: '/8.svg',
 		alt: 'eighth img',
 	},
 
 	{
 		id: 9,
-		img: '/public/9.svg',
+		img: '/9.svg',
 		alt: 'ninth img',
 	},
 
 	{
 		id: 10,
-		img: '/public/10.svg',
+		img: '/10.svg',
 		alt: 'tenth img',
 	},
 ];
